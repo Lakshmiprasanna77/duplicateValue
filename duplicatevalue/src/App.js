807f3026296a74aa4a69626f5d0472f15b79a5f9
gandla lakshmiprasanna <gandlalakshmiprasanna77@gmail.com>
2021-05-22 07:33:05 +0530

    first commit

---
+++ duplicatevalue/src/App.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react";
+class App extends Component {
+  state = {
+    dulicateNumber: "",
+  };
+  componentDidMount() {
+    let array = [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+      27,
+      28,
+      29,
+      30,
+      31,
+      32,
+      33,
+      34,
+      35,
+      36,
+      37,
+      38,
+      39,
+      40,
+      41,
+      42,
+      43,
+      44,
+      45,
+      46,
+      47,
+      48,
+      49,
+      50,
+      51,
+      52,
+      53,
+      54,
+      55,
+      56,
+      57,
+      58,
+      59,
+      60,
+      61,
+      62,
+      63,
+      64,
+      65,
+      66,
+      67,
+      68,
+      69,
+      70,
+      71,
+      72,
+      73,
+      74,
+      75,
+      76,
+      77,
+      78,
+      79,
+      80,
+      81,
+      82,
+      83,
+      84,
+      85,
+      86,
+      87,
+      88,
+      89,
+      90,
+      91,
+      92,
+      93,
+      94,
+      95,
+      96,
+      97,
+      98,
+      99,
+      100,
+    ];
+    let n = array.length;
+    let sumOfConsecutives = (n * (n + 1)) / 2;
+    let sum = array.reduce((a, b) => {
+      return a + b;
+    }, 0);
+    let difference = sumOfConsecutives - sum;
+    let duplicateValue = array[n - 1] - difference + 1;
+    this.setState({ duplicateNumber: duplicateValue });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>duplicate Number:{this.state.duplicateNumber}</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
